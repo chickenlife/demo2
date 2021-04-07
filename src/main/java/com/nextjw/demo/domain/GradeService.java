bb23c5cd63fcf63a3defdf3212c456b5f9e8fae3
deviu <deviu@deviu-GOAm1Air.local>
2021-04-07 20:52:13 +0900

    new, find person

--- src/main/java/com/nextjw/demo/domain/GradeService.java
+++ src/main/java/com/nextjw/demo/domain/GradeService.java
@@ -1,10 +1,15 @@
 package com.nextjw.demo.domain;
 
+import com.nextjw.demo.domain.model.Person;
 import com.nextjw.demo.domain.repository.GradeRepository;
 import com.nextjw.demo.domain.repository.PersonRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
 
 @Service
 @Slf4j
@@ -14,8 +19,18 @@ public class GradeService {
     private final GradeRepository gradeRepository;
 
     //find person by name
+    @Transactional(readOnly = true)
+    public Optional<Person> getPerson(String name) {
+        return personRepository.findByName(name);
+    }
 
     //person save
+    @Transactional
+    public Person addPerson(String name) {
+        Person person = new Person();
+        person.setName(name);
+        return personRepository.save(person);
+    }
 
     //grade save with person
 }
